dc95228911544227ff987975bbf9a6b49e959a0a
agarwal786 <35734063+agarwal786@users.noreply.github.com>
2021-07-24 19:03:00 +0530

    Storeserver signup api created

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,42 +1,31 @@
-const express = require("express");
-const env = require("dotenv");
+const express = require('express');
+const env = require('dotenv');
 const app = express();
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const authenticateRoutes = require('./routes/authenticate');
 
 // calling environment variables and constants
 env.config();
 
 // mongodb connection string
-//
-
 mongoose
   .connect(
     `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@store-cluster.4inst.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      useCreateIndex: true,
     }
   )
   .then(() => {
-    console.log("MongoDB connected");
+    console.log('MongoDB connected');
   });
 
 // Using Json parser
 app.use(express.json());
 
-// Sample API's
-app.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "Welcome from server",
-  });
-});
-
-app.post("/getData", (req, res, next) => {
-  res.status(200).json({
-    message: req.body,
-  });
-});
+app.use('/api', authenticateRoutes);
 
 app.listen(process.env.PORT, () => {
-  console.log("Server is running at PORT", process.env.PORT);
+  console.log('Server is running at PORT', process.env.PORT);
 });
